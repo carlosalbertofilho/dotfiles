5d9bbb064eee50eba42ecb47b0a2e4e778515222
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-04-04 16:07:17 -0300

    Init directory and config for nyxt browser

---
+++ nyxt-config/init.lisp
@@ -0,0 +1,6 @@
+(in-package #:nyxt-user)
+
+;; Import Files
+(nyxt::load-lisp "~/.config/nyxt/modules/statusline.lisp")
+(nyxt::load-lisp "~/.config/nyxt/modules/stylesheet.lisp")
+(nyxt::load-lisp "~/.config/nyxt/modules/passwd.lisp")
